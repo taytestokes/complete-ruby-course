8171a7a0459eb82f799c6dc6157391f54d071c42
Tayte Stokes <taytestokes9328@gmail.com>
2019-05-19 13:11:06 -0600

    finished the calculator

---
+++ numbers/calculator.rb
@@ -0,0 +1,29 @@
+# Mulitplication
+puts "Simple Calculator"
+25.times {print "-"}
+puts "Enter the first number:"
+num_one = gets.chomp
+puts "Enter the second number:"
+num_two = gets.chomp
+puts "The first number multiplied by the second number is: #{num_one.to_f * num_two.to_f}"
+# Addition
+25.times {print "-"}
+puts "Enter the first number:"
+num_one = gets.chomp
+puts "Enter the second number:"
+num_two = gets.chomp
+puts "The first and second number added together are: #{num_one.to_f + num_two.to_f}"
+# Subtraction
+25.times {print "-"}
+puts "Enter the first number:"
+num_one = gets.chomp
+puts "Enter the second number:"
+num_two = gets.chomp
+puts "The first and second number subtracted together are: #{num_one.to_f - num_two.to_f}"
+# Division
+25.times {print "-"}
+puts "Enter the first number:"
+num_one = gets.chomp
+puts "Enter the second number:"
+num_two = gets.chomp
+puts "The first and second number modded are #{num_one.to_f % num_two.to_f}"
---
+++ numbers/numbers.rb
@@ -0,0 +1,17 @@
+# Numbers
+puts 1 + 2
+# when an operation results in a decimal you will need to define one of the numbers as a float value
+puts 10.0 / 4
+# methods
+puts 10 / 4.to_f # this will convert 4 to a float
+# variables
+x = 5
+y = 10
+puts y / x # you can use variables to store the numbers
+
+# create a line divider
+puts "I am a line"
+20.times {print "-"}
+
+# generate random number
+20.times {puts rand(100)} # use rand() to generate a random number
