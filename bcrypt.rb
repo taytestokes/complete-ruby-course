6425da97f437bfb260b6f1b0e2a7a7e89ff76d39
Tayte Stokes <taytestokes9328@gmail.com>
2019-05-20 09:33:49 -0600

    created the auth module

--- bcrypt.rb
+++ bcrypt.rb
@@ -8,30 +8,37 @@ my_password = BCrypt::Password.create("my password")
 # use this to verify a password
 my_password = BCrypt::Password.new('$2a$10$abbXFj6bBN2QL9aaeB2jk.RObVpJ6NkhvFLRdIL91nD2GsZM1nt6q')
 
-# dummy data
-users = [
-    {username: "user1", password: "user1"},
-    {username: "user2", password: "user2"},
-    {username: "user3", password: "user3"}
-]
 
-# methods to create and verify hash
-def create_hash_digest(password)
-    BCrypt::Password.create(password)
-end
-
-def verify_hash_digest(password)
-    BCrypt::Password.new(password)
-end
-
-def create_secure_users(list_of_users)
-    # loop through users
-    list_of_users.each do |user_record|
-        # create a hashed password for the user
-        user_record[:password] = create_hash_digest(user_record[:passowrd])
+# create a module
+module Auth
+    # methods to create and verify hash
+    def create_hash_digest(password)
+        BCrypt::Password.create(password)
     end
-    # return the modified list of users
-    list_of_users
-end
 
-puts create_secure_users(users)
+    def verify_hash_digest(password)
+        BCrypt::Password.new(password)
+    end
+
+    def create_secure_users(list_of_users)
+        # loop through users
+        list_of_users.each do |user_record|
+            # create a hashed password for the user
+            user_record[:password] = create_hash_digest(user_record[:passowrd])
+        end
+        # return the modified list of users
+        list_of_users
+    end
+
+    def authenticate_user(username, password, list_of_users)
+        # loop through the users
+        list_of_users.each do |user_record|
+            # check if username and password are correct
+            if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
+                return user_record
+            end
+        end
+        # if the user is not found
+        puts "Credentials were not correct"
+    end
+end
